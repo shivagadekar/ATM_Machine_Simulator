0432a7ccca3c602fa01815317c48c4f8144f540c
Shivam <shivamgadekar1998@gmail.com>
2022-03-07 23:57:53 +0530

    change whole linear programming into functional programming

--- ATM_Machine_Simulator.py
+++ ATM_Machine_Simulator.py
@@ -1,104 +1,188 @@
-# Successfully Uploaded
-# I have made this this changes in this File, After closing browser.
-# Hi, I am Shivam Namdev Gadekar
-# This Programme is To Simulate Basic ATM Machine Operations.
-# As I Become professional, we'll upgrade this code accordingly.
-print('Please Insert Your Card!')
-print('Hello, Mr. Shivam Gadekar')
-atm_pin = 1234
-attempt = 3  # Initial attempts to enter pin, allot 3 attempts, after 3 wrong attempts Card will be blocked
-while attempt > 0:
-    # Take input from user, that is 4 Digit ATM Pin
-    pin = int(input('Enter Your Four Digit Pin : '))
-    if pin == atm_pin:
-        saving_account_balance = 165812
-        current_account_balance = 188027
-        print('Please Enter Number from Below List')
-        # Check which operation user wants to perform
-        print('1. Cash Withdraw '  # To withdraw cash from Account
-              '2. Fast Cash Withdraw '  # To withdraw, without extra steps(fast)
-              '3. View Account Balance '  # To View Current Account Balance
-              '4. Account Transition Details '  # To view History
-              '5. Reset Pin')  # To reset pin
-        choice = int(input('Enter Your Choice : '))
-        if choice == 1:
-            # from which account, you want to withdraw money(saving or Current)
-            print('Choose Account Type \n1. Saving \t \t2. Current')
-            sub_choice = int(input('Your Choice = '))
-            if sub_choice == 1:
-                amount_to_withdraw = int(input('Enter Amount to withdraw in x $100 = '))
-                if amount_to_withdraw < saving_account_balance:
-                    var1 = amount_to_withdraw % 100
-                    if var1 == 0:
-                        print('Collect Your Cash. Mr. Shivam')
-                    else:
-                        print('Enter Amounts in 100')
-                        break
-                else:
-                    print('Insufficient Account Balance.')
-                    break
-            elif sub_choice == 2:
-                amount_to_withdraw = int(input('Enter Amount to withdraw in x $100'))
-                if amount_to_withdraw < current_account_balance:
-                    var1 = amount_to_withdraw % 100
-                    if amount_to_withdraw == (var1 != 0):
-                        print('Collect Your Cash. Mr. Shivam')
-                        break
-                    else:
-                        print('Enter Amounts in 100')
-                        break
-                else:
-                    print('Insufficient Account Balance.')
-                    break
-        if choice == 2:  # Fewer Step Withdraw
-            choice = int(input('Please Choose Amount 1. 1000 2. 2000 3. Enter Your Amount'
-                               'Note : Amount will be Deduced from Saving Account'))
-            if saving_account_balance > 2000:
-                if choice == 1:
-                    print('Collect Your Cash')
-                    saving_account_balance = saving_account_balance - 1000
-                elif choice == 2:
-                    print('Collect Your Cash')
-                    saving_account_balance = saving_account_balance - 2000
-                elif choice == 3:
-                    amount_to_withdraw = int(input('Enter Amount to withdraw in x $100'))
-                    if amount_to_withdraw < saving_account_balance:
-                        if amount_to_withdraw == (amount_to_withdraw % 100 == 0):
-                            print('Collect Your Cash. Mr. Shivam')
-                            saving_account_balance = saving_account_balance - amount_to_withdraw
-                        else:
-                            print('Enter Amounts in 100')
-                    else:
-                        print('Insufficient Account Balance.')
-                        break
-        if choice == 3:
-            choice = int(input('Choose Account Type :\n1. Saving \2. Current'))
-            if choice == 1:
-                print('Your Saving Account Balance is : ', saving_account_balance, 'Dollers')
-            elif choice == 2:
-                print('Your Current Account Balance is : ', current_account_balance, 'Dollers')
+# ===============================***({ATM_MACHINE_UPDATE_V03})***==========================================
+# In this update, all Main menu's are defined under defined_functions, with their names
+# This also includes, Resend OTP feature.
+# Comparatively, Code is much easier to read and modify
+# In next version,
+# 1.    We'll try to add feature to View/add/update user data
+# 2.    Personnel information and ATM pin will be accessed with the help of CARD No only
+# 3.    Code will be able to access information of number of people, without hesitating
+# 4.    Ms-Excel may be used to store and access data
+# 5.    If statement asking for "is Card inserted", if Yes - Then Executed all code or Try again popup
+# 6.    Another Information regarding Update will be updated soon
+
+
+import time as t
+
+saving_account_balance = [200000]
+current_account_balance = [200000]
+atm_pin = [1234]
+done = False
+
+
+def card_insert_request():
+    return print('Please Insert Your Card!')
+
+
+def card_detect_msg():
+    return print('Card Detected. Please Wait Some Time for Machine To Read Card !')
+
+
+def greeting_msg():
+    return print('Hello, Mr. Shivam Gadekar')
+
+
+def main_menu():
+    print('\nPlease Enter Number from Below List\n')
+    # Check which operation user wants to perform
+    print('1. Cash Withdraw \t\t\t\t\t'  # To withdraw cash from Account
+          '2. Fast Cash Withdraw \t\t'  # To withdraw, without extra steps(fast)
+          '3. View Account Balance \n'  # To View Current Account Balance
+          '4. Account Transition Details \t\t'  # To view History
+          '5. Reset Pin \t\t\t\t'
+          '6. Quit Your Account \n')  # To reset pin
+
+
+def cash_withdraw():
+    # from which account, you want to withdraw money(saving or Current)
+    print('Choose Account Type \n1. Saving \t\t\t\t\t\t\t2. Current')
+    sub_choice = int(input('Your Choice : '))
+    if sub_choice == 1:
+        amount_to_withdraw = int(input('Enter Amount to withdraw in x $100 = '))
+        if amount_to_withdraw < saving_account_balance[0]:
+            var1 = amount_to_withdraw % 100
+            if var1 == 0:
+                t.sleep(2)
+                print('Collect Your Cash. Mr. Shivam')
+                user_choice = int(input('You want to view Account Balance : '
+                                        '1. Yes                   2. No'))
+                if user_choice == 1:
+                    saving_account_balance[0] = saving_account_balance[0] - amount_to_withdraw
+                    print('Your Account Balance is', saving_account_balance[0], 'Dollers.')
+                elif user_choice == 2:
+                    print('Thank You.')
             else:
-                print('Oops. Wrong Choice')
-        if choice == 4:
-            print("You Don't Have Any Transition Details, We'll Do it Later, to Record History")
-        if choice == 5:
-            print('Enter Your Phone Number +91-********81')
-            mobile_no = int(input('Enter Your Phone No : '))
-            if mobile_no == 9860645981:
-                attempt = 3
-                otp = int(input('OTP is sent to Registered Mobile No,Please Enter = '))
-                while attempt > 0:
-                    if otp == 1234:
-                        new_pin = int(input('Enter New Pin : '))
-                        atm_pin = new_pin
-                        break
-                    else:
-                        print('Wrong OTP')
-                        attempt -= 1
+                print('Enter Amounts in 100')
+        else:
+            print('Insufficient Account Balance.')
+    elif sub_choice == 2:
+        amount_to_withdraw = int(input('Enter Amount to withdraw in x $100'))
+        if amount_to_withdraw < current_account_balance[0]:
+            var1 = amount_to_withdraw % 100
+            if var1 == 0:
+                t.sleep(2)
+                print('Collect Your Cash. Mr. Shivam')
+                user_choice = int(input('You want to view Account Balance : '
+                                        '1. Yes                   2. No'))
+                if user_choice == 1:
+                    current_account_balance[0] = current_account_balance[0] - amount_to_withdraw
+                    print('Your Account Balance is', current_account_balance[0], 'Dollers.')
+                else:
+                    print('Thank You.')
             else:
-                print('Your Card is Blocked, Contact Bank For More Details.')
+                print('Enter Amounts in 100')
+        else:
+            print('Insufficient Account Balance.')
+
+
+def fast_cash_withdraw():
+    choice3 = int(input('Please Choose Amount 1. 1000 2. 2000 3. Enter Your Amount'
+                        'Note : Amount will be Deduced from Saving Account'))
+    if saving_account_balance[0] > 2000:
+        if choice3 == 1:
+            print('Collect Your Cash')
+            saving_account_balance[0] = saving_account_balance[0] - 1000
+        elif choice3 == 2:
+            print('Collect Your Cash')
+            saving_account_balance[0] = saving_account_balance[0] - 2000
+        elif choice3 == 3:
+            amount_to_withdraw = int(input('Enter Amount to withdraw in x $100'))
+            if amount_to_withdraw < saving_account_balance[0]:
+                if amount_to_withdraw == (amount_to_withdraw % 100 == 0):
+                    print('Collect Your Cash. Mr. Shivam')
+                    saving_account_balance[0] = saving_account_balance[0] - amount_to_withdraw
+                else:
+                    print('Enter Amounts in 100')
+            else:
+                print('Insufficient Account Balance.')
+
+
+def view_account_bal():
+    choice4 = int(input('Choose Account Type :\n1. Saving \2. Current'))
+    if choice4 == 1:
+        print('Your Saving Account Balance is : ', saving_account_balance[0], 'Dollers')
+    elif choice4 == 2:
+        print('Your Current Account Balance is : ', current_account_balance[0], 'Dollers')
     else:
-        attempt -= 1
-        if attempt == 0:
-            print('Your Card is Blocked, Contact Bank.')
-        print('You Entered Wrong Choice, Only', attempt, 'Left.')
+        print('Oops. Wrong Choice')
+
+
+def view_trans_details():
+    t.sleep(2)
+    print("You Don't Have Any Transition Details, We'll Do it Later, to Record History")
+
+
+def reset_pin():
+    print('Enter Your Phone Number +91-********81')
+    mobile_no = int(input('Enter Your Phone No : '))
+    if mobile_no == 9860645981:
+        attempt1 = 3
+        t.sleep(2)
+        otp_conf = input('Received OTP 1.\t\tYes\t\t\t\t\t\t2.\t\tNo and Resend\n')
+        if otp_conf == '1':
+            otp = int(input('OTP is sent to Registered Mobile No,Please Enter = '))
+            while attempt1 > 0:
+                if otp == 1234:
+                    new_pin = int(input('Enter New Pin : '))
+                    atm_pin[0] = new_pin
+                    break
+                else:
+                    print('Wrong OTP')
+                    attempt1 -= 1
+        elif otp_conf == "2":
+            print('Oops! Technical Problem. Please Try After Some Time')
+    else:
+        print('Wrong Mobile Number, Enter Correct Phone Number.')
+
+
+while not done:
+    card_insert_request()
+    t.sleep(1)
+    card_detect_msg()
+    t.sleep(2)
+    greeting_msg()
+    done2 = False
+    attempt = 3
+    while not done2:
+        if attempt != 0:
+            t.sleep(1)
+            pin = int(input('Enter Your Four Digit Pin : '))
+            if pin == atm_pin[0]:
+                saving_account_balance[0] = saving_account_balance[0]
+                current_account_balance[0] = current_account_balance[0]
+                t.sleep(2)
+                main_menu()
+                choice = int(input('Enter Your Choice : '))
+                if choice == 1:
+                    cash_withdraw()
+                if choice == 2:
+                    fast_cash_withdraw()
+                if choice == 3:
+                    view_account_bal()
+                if choice == 4:
+                    view_trans_details()
+                if choice == 5:
+                    reset_pin()
+                if choice == 6:
+                    done = True
+                    break
+            else:
+                if attempt == 1:
+                    t.sleep(2)
+                    print('Your Card is Blocked, Contact Bank.')
+                    done2 = True
+                attempt -= 1
+                print('You Entered Wrong Choice, Only', attempt, 'Left.')
+        else:
+            print('You Entered Wrong OTP Too many Times.')
+            done2 = True
+# Paste this 'done = True' below done2, or your machine will work continuously Asking Please Insert Card!
